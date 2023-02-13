210f9e5bf72708ab1631206abde27145bbaf32a9
Diegomendo00 <diego_121197@hotmail.es>
2023-02-13 18:10:01 -0500

    feat: creation and aplication of add to cart info component

---
+++ react/components/Totalizers.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Totalizers = () => {
+  return (
+    <div>
+      <p>tenemso x item en tu compra</p>
+      <p>total: x</p>
+    </div>
+  )
+}
+
+export default Totalizers
